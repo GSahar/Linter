7c7c85a2d35ba50182b8121dab8b2c99b348dd7d
GSahar <andrey_144_sahar@mail.ru>
2020-09-12 22:25:51 +0500

    first commit linter

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const characters = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'маг', health: 0 },
+  { name: 'лучник', health: 0 },
+];
+
+const alive = characters.filter((item) => item.health > 0);
+/*без этого ошибка:  'alive' is assigned a value but never used  no-unused-vars*/
+console.log(alive);
